4bc5fdf0ee8fc414c3b327c28999b9de66f89c37
kotik <jekaterina.guzek@gmail.com>
2024-12-02 10:40:45 +0200

    Vigade otsing

--- Vigade otsing.py
+++ Vigade otsing.py
@@ -6,7 +6,7 @@ S= a ** 2
 print("Ruudu pindala", S)
 P=4*a
 print("Ruudu ümbermõõt", P)  #было написано '' вместо "
-di=a * (2**2)  # sqr корень?
+di=a * sqrt(2) # убрала math и оставила не sqr, а sqrt
 print("Ruudu diagonaal", round(di,2))
 print() #пустая строка?
 print("Ristküliku karakteristikud") #лишния )
@@ -16,14 +16,14 @@ S=b*c
 print("Ristküliku pindala", S) #Не хватает ""
 P=2*(b+c)
 print("Ristküliku ümbermõõt", P)
-di=(b*2+c*2)**2 # sqr корень?
-print("Ristküliku diagonaal", round(di)) #Нехватало )
+di=sqrt((b**2)+(c**2)) # d di=math.sqrt(b*2+c*2) убрала math и самой формуле ** добавила
+print("Ristküliku diagonaal", round(di, 2)) #Нехватало round( , 2)
 print()  #пустая строка?
 print("Ringi karakteristikud")
 r=float(input("Sisesta ringi raadiusi pikkus => ")) #"" не правильные, не хватало float( это число может быть любое
 d=2*r #* между 2r нужно поставить действия (оператора) *
 print(f"Ringi läbimõõt {d}")   #запись не верная
-S=pi*r*2 # Pi без ()
-print("Ringi pindala", round(S))
+S=pi*(r**2) # Pi без (), ** в квадрате 
+print("Ringi pindala", round(S, 2)) # round(что, сколько после , )
 C=2*pi*r # Pi без (), * не хватало
-print("Ringjoone pikkus", round(C)) # ) не хватает
+print("Ringjoone pikkus", round(C, 2)) # ) не хватает, round(что, сколько после , )
