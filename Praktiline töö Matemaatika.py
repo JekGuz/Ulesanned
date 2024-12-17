5dcca6405c1cfea2222dfd4731dc3654cdee7994
kotik <jekaterina.guzek@gmail.com>
2024-12-17 15:24:07 +0200

    пробую, не записывает ответ! надо исправить до 17.12

--- Praktiline töö Matemaatika.py
+++ Praktiline töö Matemaatika.py
@@ -9,9 +9,9 @@ N = int(input("Сколько раз будем решать примеры: "))
 lv = int(input("Введите уровень сложности: "))
 for i in range(1,N+1,1):
     if lv == 1:
-        num1 = random.randint(1, 10)
+        num1 = random.randint(1, 10)  #random.randint(start, stop)
         num2 = random.randint(1, 10)
-        operation = random.choice(["+", "-"])
+        operation = random.choice(["+", "-"])   #Метод choice()возвращает случайно выбранный элемент из указанной последовательности, в мое случаии списка
     elif lv == 2:
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
@@ -38,8 +38,22 @@ for i in range(1,N+1,1):
     answer = float(input("Какой ответ: "))
     if answer == correct_answer:
         print("Правильно!")
+        correct += 1   #не считает почему????????? я думала запишит
     else:
         print(f"Ответ не верный, правильный ответ: {correct_answer}")
 
+hind = (correct / N) * 100
+print("\nТест завершен!")
+print(f"Правильных ответов: {correct} из {N} оценка {hind}% ")
+if hind < 60:
+    grade = "Оценка 2"
+elif 60 <= hind < 75:
+    grade = "Оценка 3"
+elif 75 <= hind < 90:
+    grade = "Оценка 4"
+else:
+    grade = "Оценка 5"
 
+print(f"Ваша оценка: {grade}")
+print("Спасибо за участие!")
 
