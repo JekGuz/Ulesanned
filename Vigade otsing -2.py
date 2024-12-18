61ff782619a7f8a6f69ec924cc1aeb2d3f43a432
Jekaterina Guzek <jekaterina.guzek@gmail.com>
2024-12-18 15:45:28 +0200

    Tehtud

--- Vigade otsing -2.py
+++ Vigade otsing -2.py
@@ -1,11 +1,11 @@
 print("*** Arvude mäng ***")
 print()
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-while 1:
+while 1:  #1 = True
     try:
-        a = abs(int(input("Sisesta täisarv => ")))
+        a = abs(int(input("Sisesta täisarv => ")))  # abs - натуральное число...
         break
-    except ValueError:
+    except ValueError: #  ValueError - тип ошибки
         print("See ei ole täisarv")
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 if a == 0:
@@ -17,16 +17,16 @@ else:
     c = b = a
     paaris = 0
     paaritu = 0
+
     while b > 0:
-            if b % 2 == 0:
-                    paaris =+ 1
-            else:
-                    paaritu =+ 1
-            b = b // 10
+        if b % 2 == 0:  #Проверяем четное / нечетное
+            paaris += 1
+        else:
+            paaritu += 1
+        b = b // 10 #// откидывает делим оставляя только четное число
     
-    print(f"Paaris: {paaris}")
-    print()
-    print(f"Paaritu: {paaritu}")
+    print("Paaris:", paaris) #print(f"Paaris: {paaris}")
+    print("Paaritu:", paaritu) #print(f"Paaritu: {paaritu}")
     print()
 #''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("*Ümberpöörame* sisestatud arvu")
@@ -36,21 +36,24 @@ else:
         number = a % 10
         a = a // 10
         b = b * 10
-        b =+ number
-    print(f"*Ümberpöörame* arv, {b}")
+        b += number
+    print("*Ümberpöörame* arv", b)
     print()
 #''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("Tõestame teoreem")
     print()
-    if c % 2 == 0:
-        print("с - Paaris. jagame 2.")
-    else:
-        print("с - Paaritu. jagame 3, liidame 1 ja jagame 2.")
+    # if c % 2 == 0:
+    #     print("с - Paaris. jagame 2.")
+    # else:
+    #     print("с - Paaritu. jagame 3, liidame 1 ja jagame 2.")
     while c != 1:  #!= не равно
         if c % 2 == 0:
+            print('{:>4}'.format(round(c))), "- Paaris arv, jagame 2."
             c = c / 2
         else:
+            print('{:>4}'.format(round(c)), "- Paaritu arv, korrutame 3, liidame 1 ja jagame 2.")
             c = (3*c + 1) / 2
-            print(c, end="\n")
+            # print(c, end="\n")
+   
+    print('{:>4}'.format(round(c)), "- Teoreem on tõestatud")
     print()
-    print("Teoreem on tõestatud")
