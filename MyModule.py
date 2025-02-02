022920f694270ced370eeb6e905b2ce4835f3e9d
«JekGuz» <jekaterina.guzek@gmail.com>
2025-02-02 22:02:07 +0200

    arrr ei saa aru

--- MyModule.py
+++ MyModule.py
@@ -1,4 +1,6 @@
-﻿
+﻿from random import *
+
+
 #Ver 0
 loginid = []
 paroolid = []
@@ -6,12 +8,48 @@ while True:
     print("Kas te olite registrerituud? (Y/N)")
     vasta1 = str(input(": "))
     if vasta1.upper() == "Y":
-        login = input("Sisesta oma login: ")
-        #kontroll listis
-        pasw = input("Siseta oma parool: ")
-        #kontroll listis
-        if login in loginid:
-            print("Õige te olite sisestatu")
+        while True:
+            login = input("Sisesta oma login: ")
+            #kontroll listis
+            pasw = input("Siseta oma parool: ")
+            #kontroll listis
+            if login in loginid:
+                print("Õige te olite sise logitud!")
+                #Vahetamine
+                print("Kas soovite vahetada paarol? (Y/N)")
+                vastus = str(input(": "))
+                if vastus.upper() == "Y":
+                    print("Soovite teha tema ise või genererida?")
+                    a = str(": ")
+                    if a.upper() == "ise":
+                        print("Palun kirjutage uus parool")
+                        p = str(input(": "))
+                        if p.isdigit() and p.isupper():
+                            print("Parool soobib!")
+                            paroolid.append(p)
+                        else:
+                            print("Paroolis peab olema tähed ja arved!")
+                    if a.upper() == "genererida":
+                        str0=".,:;!_*-+()/#¤%&"
+                        str1 = '0123456789'
+                        str2 = 'qwertyuiopasdfghjklzxcvbnm'
+                        str3 = str2.upper()
+                        print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+                        str4 = str0 +str1+str2+str3
+                        print(str4)
+                        ls = list(str4)
+                        print(ls)
+                        random.shuffle(ls) # type: ignore
+                        print(ls)
+                        # Извлечь 12 случайных значений из списка
+                        psword = ''.join([random.choice(ls) for x in range(12)]) # type: ignore
+                        # Пароль готов
+                        print(psword)
+
+
+
+
+
         else:
             print("Proovi uuesti")
 
