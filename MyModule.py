d78c0411323281020cfdc0dc58928005d79c1320
«JekGuz» <jekaterina.guzek@gmail.com>
2025-02-12 11:30:34 +0200

    prooov

--- MyModule.py
+++ MyModule.py
@@ -1,4 +1,4 @@
-﻿from random import *
+from random import *
 from string import *
 from string import punctuation
 from string import ascii_lowercase
@@ -9,8 +9,57 @@ from time import sleep
 from os import path, remove, system
 from tkinter import simpledialog as sd
 from gtts import *
+from MyModeleDef import *
 
-# #Ver 0.5
+
+
+# # Запускаем с файлом MyModeleDef --> где def от учителя.
+
+#C:/Users/marina.oleinik/source/repos/Aut_Reg/Salasõnad.txt
+salasõnad=loe_failist("salasona.txt")
+kasutajanimed=loe_failist("loginid.txt")
+while True:
+    print(kasutajanimed)
+    print(salasõnad)
+    
+    #räägimine("Tee oma valik", "et")
+    print("1-registreerimine\n2-autoriseerimine\n3-nime või parooli muutmine\n4-unustanud parooli taastamine\n5-lõpetamine\n")
+    vastus=int(input("Sisestage arv: "))
+    if vastus == 1:
+        print("Registreerimine")
+        kasutajanimed,salasõnad = registreerimine(kasutajanimed,salasõnad)
+        kirjuta_failisse("loginid.txt", kasutajanimed)
+        kirjuta_failisse("salasona.txt", salasõnad)
+        saada_kiri(kasutajanimed,salasõnad)
+
+    elif vastus == 2:
+        print("Autoriseerimine")
+        autoriseerimine(kasutajanimed,salasõnad)
+    elif vastus==3:
+        print("Nime või parooli muutmine")
+        vastus=input("Kas muudame nime, parooli või mõlemad")
+        if vastus=="nimi":
+            kasutajanimed=nimi_või_parooli_muurmine(kasutajanimed)
+        elif vastus=="parool":
+            salasõnad=nimi_või_parooli_muurmine(salasõnad)
+        elif vastus=="mõlemad":
+            print("Nimi muutmine: ")
+            kasutajanimed=nimi_või_parooli_muurmine(kasutajanimed)
+            print("Parooli muutmine: ")
+            salasõnad=nimi_või_parooli_muurmine(salasõnad)
+    elif vastus==4:
+        print("Unustanud parooli taastamine")
+
+    elif vastus==5:
+        print("Lõpetamine")
+        break
+    else:
+        print("Tundmatu valik")
+
+
+# # #Ver 0.5
+# salasõnad = loe_failist("Salasõnad.txt")
+# kasutajanimed = loe_failist("Kasutajad.txt")
 # loginid = [""]
 # paroolid = [""]
 # while True:
@@ -18,11 +67,13 @@ from gtts import *
 #     vastus = int(input(": "))
 #     # Soovime registerimine
 #     if vastus == 1:
-#         try:
+#         while True:
 #             login = input("Sisesta oma login: ")
 #             loginid.append(login)
-#         except:
-#             print("Midagi vale")
+#     ############# LISAME failid
+#             kirjuta_failisse(kasutajanimed, login)
+#             print("Teie login salvestatud")
+#             break
 #         genpsw = input("Soovite ise kirjutada salasõna? (Y/N): ")
 #         if genpsw.upper() == "Y":
 #             pasw = input("Siseta oma parool: ")
@@ -178,136 +229,3 @@ from gtts import *
 #             print("Nagemiseni!")
 #             break
 
-
-# Работа пренадлежит учителю
-def registreerimine(kasutajad:list,paroolid:list)->any:
-    """Kirjeldus
-    :param list kasutajad: Kirjeldus
-    :param list paroolid: Kirjeldus
-    :rtype: list,list
-    """
-    while True:
-        nimi=input("Mis on sinu nimi? ")
-        if nimi not in kasutajad:
-            while True:
-                parool=input("Mis on sinu parool? ")
-                flag_p=False
-                flag_l=False
-                flag_u=False
-                flag_d=False
-                if len(parool)>=8:
-                    parool_list=list(parool)
-                    for p in parool_list:
-                        if p in punctuation:
-                            flag_p=True
-                        elif p in ascii_lowercase:
-                            flag_l=True
-                        elif p in ascii_uppercase:
-                            flag_u=True
-                        elif p in digits:
-                            flag_d=True
-                    if flag_p and flag_u and flag_l and flag_d:
-                        kasutajad.append(nimi)
-                        paroolid.append(parool)
-                        räägimine("Sinu kasutajanimi on "+nimi,"et")
-                        räägimine("Sinu salasõna on "+parool,"et")
-                    break
-                else:
-                    räägimine("Nõrk salasõna!","et")
-                    print("Nõrk salasõna!")
-            break
-        else:
-            print("Selline kasutaja on juba olemas!")
-    #mail=sd.askstring("Kirjuta oma e-posti!","Kuhu saada kirja?")
-    #email(mail)
-    return kasutajad, paroolid
-def räägimine(tekst:str,keel:str):
-    obj=gTTS(text=tekst,lang=keel,slow=False).save("heli.mp3")
-    system("heli.mp3")
-def autoriseerimine(kasutajad:list,paroolid:list):
-    """Funktsioon kuvab ekraanile "Tere tulemast!" kui kasutaja on olemas nimekirjas
-        Nimi on järjendis kasutajad
-        Salasõna on paroolide järjendis
-        Nimi ja salasõna indeksid on võrdsed
-    :param list kasutajad:...
-    :param list paroolid:...
-    """
-    p=0
-    while True:
-        nimi=input("Sisesta kasutajanimi: ")              
-        if nimi in kasutajad:            
-            while True:
-                parool=input("Sisesta salasõna: ")
-                p+=1
-                try:
-                    if kasutajad.index(nimi)==paroolid.index(parool):
-                        print(f"Tere tulemast! {nimi}")
-                        break                   
-                except:
-                    print("Vale nimi või salasõna!")
-                    if p==5: 
-                        print("Proovi uuesti 10 sek pärast")
-                        for i in range(10):
-                            sleep(1)
-                            print(f"On jäänud {10-i} sek")
-        else:
-            print("Kasutajat pole")
-def nimi_või_parooli_muurmine(list_:list):
-    """
-    """
-    muutuja=input("Vana nimi või parool: ")
-    if muutuja in list_:
-        indeks=list_.index(muutuja)
-        muutuja=input("Uus nimi või parool: ")
-        list_[indeks]=muutuja
-    return list_
-def loe_failist(fail:str)->list:
-    """Funktsioon loeb tekst *.txt failist
-    """
-    f=open(fail,'r',encoding="utf-8")
-    järjend=[]
-    for rida in f:
-        järjend.append(rida.strip())
-    f.close()
-    return järjend
-def kirjuta_failisse(fail:str,järjend=[]):
-    """Salvestame tekst failisse
-    """
-    #n=int(input("Mitu: "))
-    #for i in range(n):
-    #    järjend.append(input(f"{i+1}. sõna: "))
-    f=open(fail,'w',encoding="utf-8")
-    for element in järjend:
-        f.write(element+"\n")
-    f.close()
-def ümber_kirjuta_fail(fail:str):
-    """
-    """
-    f=open(fail,'a')
-    text=input("Sisesta tekst:")
-    f.write(text+"\n")
-    f.close()
-def failide_kustutamine():
-    """
-    """
-    failinimi=input("Mis fail tahad eemaldada?") #path.isdir("Kaust")
-    if path.isfile(failinimi):
-        remove(failinimi)
-        print(f"Fail {failinimi} oli kustutatud")
-    else:
-        print(f"Fail {failinimi} puudub")
-def loe_ankeet(fail:str)->any:
-    fail=open(fail,"r",encoding="utf-8")
-    kus=[]
-    vas=[]
-    #kus_vas={}
-    for line in fail:
-        n=line.find(":")# , - разделитель
-        kus.append(line[0:n].strip())
-        vas.append(line[n+1:len(line)].strip())
-    
-        #k,v=line.strip().split(":")
-        #kus_vas[k]=v
-        
-    fail.close()
-    return kus,vas #,kus_vas
