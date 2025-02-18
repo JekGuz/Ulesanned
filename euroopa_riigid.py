cb6fee79c1fa64391ea7eb2183a29d8f46a7a13b
«JekGuz» <jekaterina.guzek@gmail.com>
2025-02-18 12:14:18 +0200

    Mingi plaan 0

--- euroopa_riigid.py
+++ euroopa_riigid.py
@@ -1,75 +1,60 @@
 from random import *
-def failist_to_dict(f:str):
-    riik_paelinn = {} # sõnastik {"Riik": "Pealinn"}
-    pealinn_riik = {} # Sõnastik {"Pealinn": "Riik"}
-    riigid1 = [] #järjend, kus talletakse riigide nimitused
-    file = open(f, 'r', encoding ="utf-8-sig")
-    for line in file:
-        k, v = line.strip().split('-') #k-võti, v - väärtus
-        riik_paelinn[k] = v #täidame riik_pealinn
-        pealinn_riik[v] = k #täidame pealinn_riik
-        riigid1.append(k)
-        file.close()
-        return riik_paelinn,pealinn_riik,riigid1
 
-#käivitamine loodud funktsion
-riik_pealinn, pealinn_riik, riigid1 = failist_to_dict("Riigid1.txt")
+riik_pealinn = {}#sõnastik {"Riik":"Pealinn"}
+def failist_to_dict(f:str):
+    riik_pealinn = {}#sõnastik {"Riik":"Pealinn"}
+    pealinn_riik = {}#sõnastik {"Pealinn":"Riik"}
+    riigid = [] #järjend, kus talletakse riigide nimetused
+    file=open(f,'r',encoding="utf-8-sig")
+    for line in file:
+        k,v = line.strip().split('-') #k-võti, v-väärtus
+        riik_pealinn[k] = v #täidame riik_pealinn
+        pealinn_riik[v] = k #täidame pealinn_riik
+        riigid.append(k)
+    file.close()
+    return riik_pealinn,pealinn_riik,riigid
+
+def lisa_faili(failinimi, riik, pealinn):
+    """Lisame uus nimi
+    Lisame uus nimi
+
+    """
+    with open(failinimi, 'a', encoding="utf-8-sig") as file:
+        file.write(f"\n{riik}-{pealinn}")
+
+#käivitame loodud funktsion
+riik_pealinn,pealinn_riik,riigid=failist_to_dict("Riigid1.txt")
 riigid = list(riik_pealinn.keys())
 
-#list pealinnad
+#list riigid
 print(riigid)
 print(riik_pealinn.keys())
 
+#list pealinnad
 pealinnad = list(riik_pealinn.values())
-
-#list riigide nimetused
 print(pealinnad)
 print(riik_pealinn.values())
 
-#prindimi riigide nimetusd
+#prindime riigide nimetused
 while True:
-    riik = input("Riik: ")
-    if riik == "A": break
-    print("Pealinn: ", riik_pealinn[riik])
+    sisend = input("Sisesta riigi või pealinna nimi (või 'A' lõpetamiseks): ")
+    if sisend == "A":
+        break
+    elif sisend in riik_pealinn:
+        print(f"Pealinn: {riik_pealinn[sisend]}")
+    elif sisend in pealinn_riik:
+        print(f"Riik: {pealinn_riik[sisend]}")
+    
+
+    # Lisame veel riik/pealinn
+    
+    
+    else:
+        print("Sellist riiki või pealinna ei leitud.")
 
 #Veerud riigid-pealinnad
-for key, value in riik_pealinn.items():
+for key,value in riik_pealinn.items():
     print(key+"-"+value+"\n")
 
 
-from random import *
-
-def failist_to_dict(f: str):
-    riik_paelinn = {}
-    pealinn_riik = {}
-    riigid = []
-
-    with open(f, 'r', encoding="utf-8-sig") as file:
-        for line in file:
-            k, v = line.strip().split('-')
-            riik_paelinn[k] = v
-            pealinn_riik[v] = k
-            riigid.append(k)
-
-    return riik_paelinn, pealinn_riik, riigid
-
-# käivitamine loodud funktsion
-riik_pealinn, pealinn_riik, riigid = failist_to_dict("Riigid1.txt")
-
-# list pealinnad
-print("Riigid:", list(riik_pealinn.keys()))
-
-# list riigide nimetused
-pealinnad = list(riik_pealinn.values())
-print("Pealinnad:", pealinnad)
-
-# prindimi riigide nimetusd
-while True:
-    riik = input("Riik: ")
-    if riik == "A": break
-    print("Pealinn:", riik_pealinn.get(riik, "Sellist riiki pole nimekirjas!"))
-
-# Вывод всех пар "страна - столица"
-for key, value in riik_pealinn.items():
-    print(f"{key} - {value}")
 
