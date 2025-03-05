962bf68b7cb720dc9ee8ddd181bd35913c08371a
«JekGuz» <jekaterina.guzek@gmail.com>
2025-03-05 12:51:22 +0200

    puhastamine ja split kellele

--- veebleht.py
+++ veebleht.py
@@ -24,7 +24,7 @@ def saada_kiri():
     Saadame kiri
     """
     global file
-    kellele = email_text.get().strip() 
+    kellele = email_text.get().strip().split(",")    # добавить несколько пользователей
     kiri = kiri_text.get("1.0", END).strip()
     smtp_server = "smtp.gmail.com"
     port = 587
@@ -67,6 +67,18 @@ def saada_kiri():
     finally:
         server.quit()
 
+def puhastamine():
+    """
+    Puhastame kõik väljad
+    """
+    global file   # обращаемся к файлу глобально
+    email_text.delete(0, END)  # удаляем значение в поле email_text  0 - начальная позиция
+    teema_text.delete(0, END)  # END – конечная позиция (весь текст будет удалён)
+    kiri_text.delete("1.0", END) # 1.0 - первая строка нулевой симфол END все удаляем
+    file = None
+    lisa_text.configure(text="...") # меняем в поле lisa_text - на ...
+
+
 aken = Tk()
 aken.geometry("600x600")
 aken.resizable(False, False)
@@ -85,7 +97,7 @@ teema_text.grid(row=1, column=1, padx=40)
 
 Label(aken, text="Lisa:", font=("Times New Roman", 18), fg="white",bg="green", padx=38).grid(row=2, column=0)
 
-lisa_text = Label(aken, text="...", font=("Times New Roman", 9), padx=38)
+lisa_text = Label(aken, text="...", font=("Times New Roman", 7), padx=38)
 lisa_text.grid(row=2, column=1, padx=40)
 
 Label(aken, text="Kiri:", font=("Times New Roman", 18), fg="white",bg="green", padx=38).grid(row=3, column=0)
@@ -94,9 +106,9 @@ kiri_text = Text(aken, font=("Times New Roman", 18), fg="black", bg="#95da96", w
 kiri_text.grid(row=3, column=1)
 
 # Кнопки добавляем
-Button(text="LISA PILT", font=("Times New Roman", 18), fg="white",bg="green", command=vali_pilt).grid(row=5, column=0)
-Button(text="SAADA", font=("Times New Roman", 18), fg="white",bg="green", command=saada_kiri).grid(row=5, column=1)
-
+Button(text="LISA PILT", font=("Times New Roman", 18), fg="white",bg="green", command=vali_pilt).place(x=100, y=300)    #.grid(row=5, column=0)
+Button(text="SAADA", font=("Times New Roman", 18), fg="white",bg="green", command=saada_kiri).place(x=240, y=300)        #.grid(row=5, column=1)
+Button(text="PUHASTA", font=("Times New Roman", 18), fg="white",bg="red", command=puhastamine).place(x=350, y=300)       #.grid(row=5, column=2)
 
 
 aken.mainloop()
